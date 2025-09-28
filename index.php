62901ab62fa84a59b7bd68bb40a7e42b019ec277
Tiedeldze045 <99605377+Tiedeldze045@users.noreply.github.com>
2025-09-29 00:58:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ignore_user_abort(true);
 set_time_limit(0);
 flush();
 
-$token = "8053381152:AAF29eX69ZIoAURqcviADw2a1HQdYZlDo_A";
+$token = "7422219549:AAHgTws9iHWCXDa0JNkTW4nC8k1n4smCghs";
 
 $raw = file_get_contents("php://input");
 $update = json_decode($raw, true);
@@ -19,3 +19,4 @@ if ($chat_id) {
 
 exit;
 ?>
+
